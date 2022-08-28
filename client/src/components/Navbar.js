dc5d986f4646bcbc3866e6921dffc22c79a44e5e
Angel Zhang <anzhouzhang913@gmail.com>
2022-08-28 10:45:30 -0400

    Use HashLink to scroll to sections with specific id

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
 
 const Navbar = (props) => {
     return (
         <header>
             {props.texts.map((text, key) => {
-                return text === "home" || text === "gallery" ? <Link to={"/" + text} key={key}> {text} </Link> :  <a href={"/#" + text} key={key}> {text} </a>
+                return text === "home" || text === "gallery" ? <Link to={"/" + text} key={key}> {text} </Link> :  <HashLink to={"/#" + text} key={key}> {text} </HashLink>
             })}
         </header>
     )
