decd41462e68c3428dcb0033212611bdf0d2feaf
Angel Zhang <anzhouzhang913@gmail.com>
2022-08-29 15:35:32 -0400

    Add images to gallery

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const Gallery = () => {
+  const [images, setImages] = useState([])
+  const importAll = r => {
+    return r.keys().map(r)
+  }
+
+  useEffect(() => {
+    setImages(importAll(require.context('../assets/images/', false, /\.jpg$/)))
+    console.log(images)
+  }, [])
+    
+
   return (
-    <div>Gallery</div>
+    <>
+      <header>Gallery</header>
+      {images.map((image, key) => {
+        return <img src={image} alt="" key={key} style={{height:200, width:200, padding:10}}/>
+      })}
+    </>
   )
 }
 
