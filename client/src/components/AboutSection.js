349663b02caa5158c22462731b20fc6f27a40bb0
Angel Zhang <anzhouzhang913@gmail.com>
2022-09-02 22:48:46 -0400

    Change styles

--- client/src/components/AboutSection.js
+++ client/src/components/AboutSection.js
@@ -4,7 +4,7 @@ import { Card } from 'antd';
 
 const AboutSection = (props) => {
     return (
-        <section id="about" title="About Me" className="justify-content-center w-10 m-3 p-5 border-round-md">
+        <section id="about" title="About Me" className="justify-content-center w-10 mx-3 mt-5 mb-3 p-5 border-round-md">
             <SectionTitle text="About Me"/>
             <p>
                 Hello! My name is Angel Zhang, and I am a 3rd year Computer Science student at the University of Waterloo. I had completed internships as a Software Engineer at Huawei and as an IT support specialist at Tangam Systems. I am currently seeking Fall 2023 opportunities!
