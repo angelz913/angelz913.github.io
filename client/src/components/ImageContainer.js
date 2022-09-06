9b9c241c49227122689f5c7f6a11edff559fd821
Angel Zhang <anzhouzhang913@gmail.com>
2022-09-05 22:52:16 -0400

    Use ImageList for Gallery

--- client/src/components/ImageContainer.js
+++ client/src/components/ImageContainer.js
@@ -1,12 +1,22 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import ImageList from '@mui/material/ImageList'
+import ImageListItem from '@mui/material/ImageListItem'
 
 const ImageContainer = (props) => {
     return (
-        <div className="imgContainer flex flex-wrap justify-content-center pt-6">
-            {props.images.map((image, key) => {
+        <div className="imgContainer pt-6">
+            {/* {props.images.map((image, key) => {
             return <img src={image} alt="" key={key} style={{height:200, width:200, padding:10}}/>
-            })}
+            })} */}
+            <ImageList cols={2}>
+                {props.images.map((image, key) => {
+                    return (
+                        <ImageListItem key={key}>
+                            <img src={image} alt="" />
+                        </ImageListItem>
+                    )
+                })}                
+            </ImageList>
         </div>
     )
 }
